3ecdce8cc0285ef25753a9ecc619991d886b7b44
safae <safae2@dell-PC>
2017-04-01 16:21:43 +0000

    add comment by develop 2

--- src/safaetest/App.java
+++ src/safaetest/App.java
@@ -4,7 +4,9 @@ public class App {
 	public static void main(String args[]){
 		System.out.println("ok");
 		System.out.println("modifying");	
-
+		
+		// add another comment by develop 2  
+		
 		System.out.println("develop2");
 		System.out.println("develop1");
 		System.out.println("develop1 ok");	
