42896d4fa8985b4d624ea734f0760ce4c50e9d5c
safae <safae@dell-PC>
2017-04-01 16:04:45 +0000

    second commit

--- src/safaetest/App.java
+++ src/safaetest/App.java
@@ -4,7 +4,8 @@ public class App {
 	public static void main(String args[]){
 		System.out.println("ok");
 		System.out.println("modifying");	
-		System.out.println("develop1");	
+		System.out.println("develop1");
+		System.out.println("develop1 ok");	
 	}
 
 }
