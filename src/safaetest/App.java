c18d23d50b79a3c9b4a5671926d4473dcddbe842
safae <safae@dell-PC>
2017-04-01 16:36:51 +0000

    add develop1

--- src/safaetest/App.java
+++ src/safaetest/App.java
@@ -2,15 +2,7 @@ package safaetest;
 
 public class App {
 	public static void main(String args[]){
-		//add comment
-		System.out.println("ok");
-		System.out.println("modifying");	
 		System.out.println("develop1");
-		System.out.println("develop1 ok");	
-		
-		System.out.println("teeeeeesting");
-		System.out.println("teeeeeesting");
-		
 	}
 
 }
